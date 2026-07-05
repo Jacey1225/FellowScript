2e665b23ca12bc001f94e30afcafb2bbe3fa3de0
Jacey Simpson <jaceysimps@gmail.com>
2026-07-05 02:14:54 -0700

    notifications

---
+++ FellowScript/FellowScript/FellowScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  FellowScriptApp.swift
+//  FellowScript
+//
+//  Created by Jacey Simpson on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FellowScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FellowScript/FellowScriptUITests/FellowScriptUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  FellowScriptUITestsLaunchTests.swift
+//  FellowScriptUITests
+//
+//  Created by Jacey Simpson on 7/1/26.
+//
+
+import XCTest
+
+final class FellowScriptUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
